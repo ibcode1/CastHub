7bf6e1d9c6c523218f973e821bdfb23edabdfdfa
Ibrahim <ibrahimfuseini88@yahoo.com>
2025-12-09 16:23:54 +0000

    Initial Commit

---
+++ CastHub/CastHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  CastHubApp.swift
+//  CastHub
+//
+//  Created by Ibrahim fuseini on 09/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct CastHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
